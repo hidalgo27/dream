e3d004a71d7fda97026b77e7dbc99a31396be404
Hidalgo <hidalgochpnce@gmail.com>
2018-10-09 19:40:58 -0500

    Se creo la base de la aplicacion

--- resources/assets/js/font-awesome.js
+++ resources/assets/js/font-awesome.js
@@ -4,7 +4,7 @@
 import fontawesome from '@fortawesome/fontawesome';
 import solid from '@fortawesome/fontawesome-free-solid';
 import regular from '@fortawesome/fontawesome-free-regular';
-// import brands from '@fortawesome/fontawesome-free-brands';
+import brands from '@fortawesome/fontawesome-free-brands';
 
 fontawesome.library.add(solid);
 fontawesome.library.add(regular);
